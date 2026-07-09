8ffb39463e8b3d8d8ef91ac3c7ddf6c122b9c9a8
Gustavo Assis Machado Guimarães <gustavoamg@gmail.com>
2026-07-09 16:37:00 +0100

    Setup Hilt

---
+++ app/src/androidTest/java/pt/viabilize/blissandroidchallenge/di/HiltConfigurationTest.kt
@@ -0,0 +1,41 @@
+package pt.viabilize.blissandroidchallenge.di
+
+import android.content.Context
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import javax.inject.Inject
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+class HiltConfigurationTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    @ApplicationContext
+    lateinit var context: Context
+
+    @Before
+    fun init() {
+        // Executa a injeção de dependências no objeto de teste atual
+        hiltRule.inject()
+    }
+
+    @Test
+    fun verifyHiltInjection_ContextShouldNotBeNull() {
+        // Se o Hilt estiver mal configurado, o teste falhará antes ou aqui
+        assertNotNull(context)
+
+        // Verifica se o contexto recuperado corresponde ao pacote correto do seu app
+        assertEquals("pt.viabilize.blissandroidchallenge", context.packageName)
+    }
+}
---
+++ app/src/main/java/pt/viabilize/blissandroidchallenge/BlissApplication.kt
@@ -0,0 +1,8 @@
+package pt.viabilize.blissandroidchallenge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BlissApplication: Application () {
+}
